acb0377e81da8da5b541e849f7b8ec02d8f67a0c
NotXor2007 <adamnaanaa1234gb@gmail.com>
2024-09-13 13:48:01 +0200

    adding shell.py & settings.py

--- widgets/shell.py
+++ widgets/shell.py
@@ -1,11 +1,14 @@
 from const import*
+import tkinter as tk
 
 class Shell:
 
 	def __init__(self, win):
 		self.win = win
 		self.shell = self.win.out.console
-		self.waitfcmd()
+		self.waitfcmd(None)
+		self.shell.bind("<Return>", self.waitfcmd)
 
-	def waitfcmd(self):
-		self.shell.insert("end", "==> ")
+	def waitfcmd(self, event):
+		cpos = self.shell.index(tk.INSERT)
+		self.shell.insert("end", "\n==> ")
